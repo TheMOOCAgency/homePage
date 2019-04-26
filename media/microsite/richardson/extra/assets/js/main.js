3dc8c02223d8002088644bee653ac605902fc5f3
root <root@vps679998.ovh.net>
2019-04-26 16:46:13 +0200

    first commit

---
+++ media/microsite/richardson/extra/assets/js/main.js
@@ -0,0 +1,8 @@
+var wmBox = function(){
+		$('body').prepend('<div class="wmBox_overlay"><div class="wmBox_centerWrap"><div class="wmBox_centerer"><div class="wmBox_contentWrap"><div class="wmBox_scaleWrap"><div class="wmBox_closeBtn"><p>x</p></div>');
+	};
+
+
+$(document).ready(function(){
+wmBox();
+});
